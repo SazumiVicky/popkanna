c4457d6a3ce0e080c2c9733938885fdfad3edd47
SazumiVicky <75471215+SazumiVicky@users.noreply.github.com>
2021-09-24 23:35:54 -0700

    Update sazumi.js

--- sazumi.js
+++ sazumi.js
@@ -6,7 +6,6 @@ var MyScore = 1;
 var audio = new Audio('pop.mp3');
 var hk_score = 1000, tw_score = 430, th_score = 200, jp_score = 150, fi_score = 250, se_score = 100, pl_score = 500, dm_score = 280, id_score=590, hu_score=319, sr_score = 300; 
 
-// mouseclick event
 img.addEventListener("mousedown", function(){
     increaseScore();
     img.src = 'https://code.sazumivicky.my.id/kanna1';
@@ -18,7 +17,6 @@ img.addEventListener("mouseup", function(){
     audio.play();
 });
 
-// touch event
 img.addEventListener("touchstart", function(){
     increaseScore();
     img.src = 'https://code.sazumivicky.my.id/kanna1';
@@ -31,7 +29,6 @@ img.addEventListener("touchmove", function(){
 });
 
 
-// Score on leaderboard
 setInterval(startCountHk, 1);
 
 function startCountHk(){
